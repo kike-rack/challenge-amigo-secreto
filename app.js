d8199ea2ff9900bfd182ef9b62afb191adb6e615
Enrique Fuenzalida <enriquefr70@gmail.com>
2025-08-30 22:44:52 -0400

    Carga HTML y CSS Final y JS base

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log(Creando el JS);
+//Diagrama
+//Usuario ingresa nombres de amigos y luego hace un sorteao aleatorio poara hacer un amigo secreto.
+//Se escribe en input un nombre y luego se presiona el boton añadir para hacer una lista en pantalla
+//Si un usuario escribe un nombre vacio, debe hacer una alerta que avise que el nombre no es válido
+//Al clickear el boton ssortear amigo, la aplicacion elije un nombre aleatorio y lo muestra en pantalla
+console.log("Creando el JS");
