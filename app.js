b2e82414cc30b9906545475eb10738d705ea0859
Enrique Fuenzalida <enriquefr70@gmail.com>
2025-08-30 23:46:28 -0400

    Agregar amigo a la lista de amigos

--- app.js
+++ app.js
@@ -3,4 +3,31 @@
 //Se escribe en input un nombre y luego se presiona el boton añadir para hacer una lista en pantalla
 //Si un usuario escribe un nombre vacio, debe hacer una alerta que avise que el nombre no es válido
 //Al clickear el boton ssortear amigo, la aplicacion elije un nombre aleatorio y lo muestra en pantalla
-console.log("Creando el JS");
+let listaAmigos = []; //Array
+let numeroMaximo = 6;
+
+//capturar el valor del campo de entrada, verifica que haya algun nombre y luego agrega el nombre a la lista
+function agregarAmigo () {
+    let nombreAmigo = document.getElementById('amigo').value;
+    document.getElementById('amigo'). value = "";
+    if (nombreAmigo === "") {
+        alert("Debes escribir un nombre.");
+        return
+    }
+    listaAmigos.push(nombreAmigo);
+    console.log(nombreAmigo);
+    limpiarCaja();
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function limpiarCaja() {
+    //capturar id del input, en este caso es amigo
+    document.querySelector('#amigo').value = '';
+}
+
+
